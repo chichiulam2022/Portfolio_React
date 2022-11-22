ec5249188a27c49a4e34bdcd4154efd4610f1b0b
Chi Chiu Lam <lamcc90@gmail.com>
2022-11-22 18:58:09 -0500

    loaders added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import '../src/styles/App.css';
 import NavBar from './components/Navbar';
 import Hero from './components/Hero'
@@ -7,24 +8,58 @@ import Aboutme from './components/Aboutme';
 import Contactme from './components/Contactme';
 import Footer from './components/Footer';
 import { Parallax, ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
+import PropagateLoader from "react-spinners/PropagateLoader";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return (
-    <ParallaxProvider>
-      <NavBar />
-      <ParallaxBanner>
-        <Parallax speed={-30}>
-          <Hero />
-        </Parallax>
-        <Skills />
-        <Projects />
-        <Aboutme />
-        <Parallax speed={-10}>
-          <Contactme />
-          <Footer />
-        </Parallax>
-      </ParallaxBanner>
-    </ParallaxProvider>
+    <div className='App'>
+      {
+        loading ?
+          <div className='loader-bx'>
+            <PacmanLoader
+              size={30}
+              color="purple"
+              className='loader'
+            />
+            <h2 className='loader-text'>Loading...</h2>
+            <PacmanLoader
+              size={30}
+              color="purple"
+              className='loader'
+            />
+          </div>
+
+          :
+          <ParallaxProvider>
+            <NavBar />
+            <ParallaxBanner>
+              <Parallax speed={-30}>
+                <Hero />
+              </Parallax>
+              <Skills />
+              <Projects />
+              <Aboutme />
+              <Parallax speed={-10}>
+                <Contactme />
+                <Footer />
+              </Parallax>
+            </ParallaxBanner>
+          </ParallaxProvider>
+      }
+    </div>
   );
 }
 
