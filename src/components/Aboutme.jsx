5aa36222919d4289a2606ff119090b2a35e6701d
Chi Chiu Lam <lamcc90@gmail.com>
2022-11-23 22:12:59 -0500

    Update Aboutme.jsx

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,60 +1,3 @@
-// import React from 'react'
-// import 'animate.css'
-// import '../styles/Aboutme.css'
-// import {useTranslation} from 'react-i18next'
-// import profilepic from '../assets/images/profile_pic_me.jpg'
-// import {BsFillFileEarmarkPdfFill} from 'react-icons/bs'
-// import {ImFolderDownload} from 'react-icons/im'
-// import TrackVisibility from 'react-on-screen';
-
-// //three.js
-// import { Canvas } from '@react-three/fiber'
-// import { Stars } from '@react-three/drei'
-
-// function Aboutme() {
-//   const { t } = useTranslation(["aboutme"]);
-
-// return (
-// <section className="aboutme px-4">
-//       <div className="container">
-//         <div className="row">
-//           <div className="col-12">
-//             <TrackVisibility>
-//               {({ isVisible }) => (
-//                 <div
-//                   className={`aboutme-bx
-//                       ${isVisible ? "" : "hidden"}`}
-//                   id="project"
-//                 >
-//                   <h2>{t("title")}</h2>
-//                   <p>{t("text1")}</p>
-//                   <p>
-//                  {t("text3")}{" "}
-//                  <button>
-//                    <a href="/resume.pdf" download>
-//                       {t("resume")}&nbsp;
-//                       <BsFillFileEarmarkPdfFill />&nbsp;
-//                       <ImFolderDownload />
-//                     </a>
-//                   </button>{" "} {t("engonly")}.
-//                   </p>
-//                   <img src={profilepic} alt="cow" className='col-4'/>  
-//                 </div>
-//               )}
-//             </TrackVisibility>
-//           </div>
-//         </div>
-//       </div>
-//       <Canvas style={{ height: "40vh", width: "100vw", opacity: "1" }}>
-//      <Stars count={1240} radius={100} saturation={0} speed={3} depth={20} />
-//      </Canvas>
-//     </section>
-    
-// )}
-
-// export default Aboutme;
-
-
 import React from "react";
 import "animate.css";
 import "../styles/Aboutme.css";
@@ -63,6 +6,7 @@ import profilepic from "../assets/images/profile_pic_me.jpg";
 import { BsFillFileEarmarkPdfFill } from "react-icons/bs";
 import { ImFolderDownload } from "react-icons/im";
 import TrackVisibility from "react-on-screen";
+import 'animate.css'
 
 //three.js
 import { Canvas } from "@react-three/fiber";
@@ -80,7 +24,7 @@ function Aboutme() {
               {({ isVisible }) => (
                 <div
                   className={`aboutme-bx
-                      ${isVisible ? "" : "hidden"}`}
+                      ${isVisible ? "animate__animated animate__flipInY" : "hidden"}`}
                   id="project"
                 >
                   <h2>{t("title")}</h2>
